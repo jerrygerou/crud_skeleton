e6e56fe64415a3dc78eb1c07673b1e66e8177aab
Sara Gerou <saragerou@gmail.com>
2017-04-10 15:22:07 -0500

    CRUD skeleton working just how I want it to

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -6,7 +6,7 @@ post '/sessions' do
   @user = User.authenticate(params[:username], params[:password])
   if @user
     session[:user_id] = @user.id
-    redirect '/'
+    erb :'users/show'
   else
     @errors = ["That user does not exist."]
     redirect '/sessions/new'
