8710270a95e6ea89bb8ca6f032e1744bcc088b37
Sara Gerou <saragerou@gmail.com>
2017-04-10 15:13:46 -0500

    Have security working for show page

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,12 +3,12 @@ get '/users/new' do
 end
 
 post '/users' do
-  return redirect "/" if logged_in?
+  return redirect '/' if logged_in?
   user = User.new(params[:user])
 
   if user.save
     session[:user_id] = user.id
-    redirect "/"
+    redirect '/'
   else
     @errors = user.errors.full_messages
     erb :'users/new'
@@ -18,5 +18,11 @@ end
 get '/users/:id' do
  
   @user = User.find(params[:id])
-  erb :'users/show' 
+  
+  if @user.id == current_user.id
+    erb :'users/show'
+  else
+    redirect '/'
+  end
+
 end
